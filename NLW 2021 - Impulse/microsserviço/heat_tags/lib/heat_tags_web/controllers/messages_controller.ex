63defabb97043e980c0fe48567d64b5b6c2de5e0
skylucacius <skylucacius@hotmail.com>
2022-05-02 19:51:12 -0300

    pequena pausa

--- NLW 2021 - Impulse/microsserviço/heat_tags/lib/heat_tags_web/controllers/messages_controller.ex
+++ NLW 2021 - Impulse/microsserviço/heat_tags/lib/heat_tags_web/controllers/messages_controller.ex
@@ -2,6 +2,12 @@ defmodule HeatTagsWeb.MessagesController do
   use HeatTagsWeb, :controller
 
   def create(conn, params) do
+    IO.inspect(params)
+
+    # conn
+    # |> text("recebi a requisição")
+
+    text(conn, "recebi a requisição")
     
   end
 end
